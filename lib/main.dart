4854675fca13841d1c92ae6915503ff4840f358e
ORBEN4 <85984457+ORBEN4@users.noreply.github.com>
2021-07-31 14:46:11 +0300

    re-orgenized the project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'projects/built_in/body.dart';
+import 'projects/project1/body.dart';
+import 'projects/project2/body.dart';
+
+
+void main() => runApp(getApp(0));
+
+StatelessWidget getApp(int version){
+  if(version == 0)
+    return BuiltInBody();
+  else if(version == 1)
+    return Project1Body();
+  else if(version == 2)
+    return Project2Body();
+  else
+    throw UnimplementedError();
+}
---
+++ lib/projects/project1/body.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import "package:flutter/material.dart";
+
+import 'const.dart';
+
+class Project1Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "My Flutter Application",
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "my first AppBar",
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: CostumeColors.yellow,
+            ),
+            body: FirstScreen()));
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          "your lucky number is ${generateLuckyNumber()}",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.blueGrey, fontSize: 30.0),
+        ),
+      ),
+    );
+  }
+
+  int generateLuckyNumber() {
+    return Random().nextInt(100);
+  }
+}
