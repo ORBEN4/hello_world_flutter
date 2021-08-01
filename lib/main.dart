901fc92c435e98008246d0ffba526d9d16e2fc2f
ORBEN4 <orbenshloosh@gmail.com>
2021-08-01 19:09:14 +0300

    added version 2.1 to versionFactory

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,20 @@ import 'package:flutter/material.dart';
 import 'projects/built_in/body.dart';
 import 'projects/project1/body.dart';
 import 'projects/project2/body.dart';
+import 'projects/project2/Home.dart';
 
 
-void main() => runApp(getApp(2));
+void main() => runApp(getApp(2.1));
 
-StatelessWidget getApp(int version){
+StatelessWidget getApp(double version){
   if(version == 0)
     return BuiltInBody();
   else if(version == 1)
     return Project1Body();
   else if(version == 2)
     return Project2Body();
+  else if(version ==2.1)
+    return Project2_1Body();
   else
     throw UnimplementedError();
 }
